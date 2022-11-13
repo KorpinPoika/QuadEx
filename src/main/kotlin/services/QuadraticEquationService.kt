e385280363f5715e3e1a4a7b75e497e5657a7abf
Alexey Musti <alexey.musti@gmail.com>
2022-11-14 00:35:07 +0300

    optimize the calculation

--- src/main/kotlin/services/QuadraticEquationService.kt
+++ src/main/kotlin/services/QuadraticEquationService.kt
@@ -1,22 +1,26 @@
 package services
 
 import entities.ComplexNumber
+import kotlin.math.abs
 import kotlin.math.pow
 import kotlin.math.sqrt
 
 class QuadraticEquationService : IQuadraticEquationService {
     override suspend fun solve(a: Double, b: Double, c: Double): Pair<ComplexNumber, ComplexNumber> {
+        val minus_b_div_2a = -b / (2*a)
         val d = b.pow(2) - 4*a*c
+        val sqrtD_div_2a = sqrt(abs(d)) / (2*a)
 
         if (d < 0) {
-            val res1 = ComplexNumber(-b/(2*a), sqrt(-d) /(2*a))
-            return Pair(res1, ComplexNumber(res1.re, -res1.im))
+            return Pair(
+                ComplexNumber(minus_b_div_2a,  sqrtD_div_2a),
+                ComplexNumber(minus_b_div_2a, -sqrtD_div_2a)
+            )
         }
 
-        val sqrtD = sqrt(d)
         return Pair(
-            ComplexNumber((-b + sqrtD) / (2*a)),
-            ComplexNumber((-b - sqrtD) / (2*a))
+            ComplexNumber(minus_b_div_2a + sqrtD_div_2a),
+            ComplexNumber(minus_b_div_2a - sqrtD_div_2a)
         )
     }
 
